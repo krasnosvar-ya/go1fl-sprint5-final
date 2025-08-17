51022157469507f61a2cbba2d1881b91995e927b
Denis K <krasnosvar@ya.ru>
2025-08-17 10:53:12 +0400

    YA sprint 5 final task

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,34 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	// Parse принимает строку данных и наполняет структуру
+	Parse(data string) error
+	// ActionInfo формирует строку с информацией об активности
+	ActionInfo() (string, error)
 }
 
+// Функция принимает слайс строк с данными о тренировках или прогулках и
+// экземпляр одной из ваших структур Training или DaySteps.
+// Это возможно, потому что они обе реализуют интерфейс DataParser,
+// то есть для каждой из этих структур вы реализовали методы, которые описаны в интерфейсе.
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, data := range dataset {
+		if err := dp.Parse(data); err != nil {
+			log.Printf("parse error: %v", err)
+			continue
+		}
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Printf("action info error: %v", err)
+			continue
+		}
+		if info != "" {
+			fmt.Println(info)
+		}
+	}
 }
--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,23 @@
 package personaldata
 
+import "fmt"
+
 type Personal struct {
 	// TODO: добавить поля
+	// Создайте экспортируемую структуру Personal, у которой три поля:
+	// Name — имя пользователя;
+	// Weight— вес пользователя;
+	// Height — рост пользователя.
+	// Эта структура будет встроена в другие структуры.
+	// Для этой структуры нужно будет создать метод, который будет выводить данные структуры на экран.
+	Name   string
+	Weight float64
+	Height float64
 }
 
 func (p Personal) Print() {
 	// TODO: реализовать функцию
+	fmt.Printf("Имя: %s\n", p.Name)
+	fmt.Printf("Вес: %.2f кг.\n", p.Weight)
+	fmt.Printf("Рост: %.2f м.\n", p.Height)
 }
